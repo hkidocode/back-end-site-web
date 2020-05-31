c09a7edbc06e0677ed1a777a322e7c636466efb2
hkidocode <57941226+hkidocode@users.noreply.github.com>
2020-05-31 23:36:45 +0100

    add footer.php

--- index.php
+++ index.php
@@ -112,15 +112,7 @@
     </div>
     <!-- /.container -->
 
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <p>Copyright &copy; The Perfect Cup 2019</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+   
 
     <!-- jQuery -->
     <script src="js/jquery.js"></script>
